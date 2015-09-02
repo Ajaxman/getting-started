ff33361f28371c9c897e75bcb22f0e9df07f4628
Javier Lopez Lopez <sjavierlopez@gmail.com>
2015-09-02 11:28:07 -0500

    Step-2: Adding controller

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-var app = angular.module('store', []);
+(function(){
+    var app = angular.module('store', []);
+
+    app.controller('StoreController', function(){
+        this.product = gem;
+    });
+
+    var gem = {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: 'this is tha description of this product'
+    }
+})();
