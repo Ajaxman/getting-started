84065f4e7728474fa83a45f329148c29e0654a7e
Javier Lopez Lopez <sjavierlopez@gmail.com>
2015-09-03 16:04:50 -0500

    Add service  and using data from json file (Api)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 (function(){
     var app = angular.module('store', ['store-products']);
 
-    app.controller('StoreController', function(){
-        this.products = gems;
-    });
+    app.controller('StoreController',['$http', function($http){
+
+        var store = this;
+
+        store.products = [];
+
+        $http.get('/angularjs/getting-started/products.json').success(function(data){
+            store.products = data;
+        });
+    }]);
 
     app.controller('ReviewController', function(){
         this.review = {};
@@ -15,63 +22,4 @@
         };
     });
 
-
-    var gems =[
-        {
-            name: 'Dodecahedron',
-            price: 2.95,
-            description: 'this is tha description of this product',
-            canPurchase: true,
-            soldOut: false,
-            images:[
-                {
-                    full: 'assets/full.jpg',
-                    thumb: 'assets/thumb.jpg'
-                },
-                {
-                    full: 'assets/full.jpg',
-                    thumb: 'assets/thumb.jpg'
-                }
-            ],
-            reviews: [{
-                stars: 3,
-                body: "I think this gem was just OK, could honestly use more shine, IMO.",
-                author: "JimmyDean@example.org",
-                createdOn: 1397490980837
-            }, {
-                stars: 4,
-                body: "Any gem with 12 faces is for me!",
-                author: "gemsRock@example.org",
-                createdOn: 1397490980837
-            }]
-        },
-        {
-            name: 'Pentagonal Gem',
-            price: 5.95,
-            description: 'this is tha description of this product',
-            canPurchase: false,
-            soldOut: false,
-            images:[
-                {
-                    full: 'assets/full.jpg',
-                    thumb: 'assets/thumb.jpg'
-                },
-                {
-                    full: 'assets/full.jpg',
-                    thumb: 'assets/thumb.jpg'
-                }
-            ],
-            reviews: [{
-                stars: 3,
-                body: "I think this gem was just OK, could honestly use more shine, IMO.",
-                author: "JimmyDean@example.org",
-                createdOn: 1397490980837
-            }, {
-                stars: 4,
-                body: "Any gem with 12 faces is for me!",
-                author: "gemsRock@example.org",
-                createdOn: 1397490980837
-            }]
-        }
-    ];
 })();
